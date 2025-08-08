f32961f0e99c2337965d70b7349b58fb17fa7297
Jun Zeng <jzeng.home@gmail.com>
2025-08-08 03:37:21 +0000

    working version

--- rcnn/SM_Driver.py
+++ rcnn/SM_Driver.py
@@ -1,38 +1,24 @@
-import os
-import torch
-import sagemaker
 from sagemaker.pytorch import PyTorch
+from sagemaker import get_execution_role
+import sagemaker
 
-# Detect environment
-SIM_SM = os.environ.get("SM_CHANNEL_TRAINING") is None
-print("Running in Local Mode" if SIM_SM else "Running in SageMaker Cloud")
+print("Running with Sagemaker.")
 
-# Role (unused in Local Mode)
-role = "arn:aws:iam::123456789012:role/FakeRole" if SIM_SM else "arn:aws:iam::010819239854:role/service-role/AmazonSageMaker-ExecutionRole-MyRole"
-
-# Session
-session = sagemaker.local.LocalSession() if SIM_SM else sagemaker.Session()
-if SIM_SM:
-    session.config = {'local': {'local_code': True}}
-
-# Instance type
-instance = "local_gpu" if SIM_SM and torch.cuda.is_available() else ("local" if SIM_SM else "ml.m5.large")
- #    ml.m5.large = cheap CPU, ml.g4dn.xlarge = GPU
+role = get_execution_role()
+session = sagemaker.Session()
 
 estimator = PyTorch(
     entry_point="train.py",
+    source_dir=".",                   # current directory
     role=role,
     framework_version="1.13",
     py_version="py39",
     instance_count=1,
-    instance_type=instance,
+    instance_type="ml.m5.large",
     sagemaker_session=session,
-    dependencies=["requirements.txt"]
+    dependencies=["requirements.txt"],
+    base_job_name="jzeng-rcnn"
 )
 
-# Training
-if SIM_SM:
-    estimator.fit({"training": "file://./data/PennFudanPed"})
-else:
-    s3_input = session.upload_data(path="./data", key_prefix="pytorch-demo/data")
-    estimator.fit({"training": s3_input})
+s3_input = "s3://my-sagemaker-bucket-jzeng/PennFudanPed/"
+estimator.fit({"training": s3_input})
